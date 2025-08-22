4d36a0ed2517b65e7a3dbe9effef473371598e6a
YOUSEF <ghhjhkkoooppppplll@gmail.com>
2025-08-22 18:55:54 +0300

    DEPLOY

--- app/Instractot/layout.tsx
+++ app/Instractot/layout.tsx
@@ -1,58 +1,50 @@
 'use client'
-import { ReactNode, useState } from 'react';
-import { AddressBook } from 'tabler-icons-react';
+import { ReactNode, useState } from 'react'
+import { AddressBook } from 'tabler-icons-react'
 
 import {
   FaHome,
   FaUsers,
-  FaQuestionCircle,
   FaChartBar,
   FaChalkboardTeacher,
 } from 'react-icons/fa'
 import { MdGroups } from 'react-icons/md'
-import Sidebar from '../sidebar/Sidebar';
-import Navbar from '../navbar/navbar';
-import Protected_roote from '../Protected_roote';
-interface menuItems{
-  
-    name:string
-    icon:ReactNode;
-    active?:boolean
- 
+
+import Sidebar from '../sidebar/Sidebar'
+import Navbar from '../navbar/navbar'
+import Protected_roote from '../Protected_roote'
+
+interface MenuItem {
+  name: string
+  icon: ReactNode
+  active?: boolean
+  path: string
 }
-const menuItems = [
-  { name: 'Dashboard', icon: <FaHome /> ,path:'/Instractot/dashboard'},
-  { name: 'Students', icon: <FaUsers />, path:'/Instractot/students' },
-  { name: 'Groups', icon: <MdGroups />, path:'/Instractot/groups'},
-  { name: 'Quizzes', icon: <FaChalkboardTeacher />, path:'/Instractot/quizzes' },
-  { name: 'Results', icon: <FaChartBar />, path:'/Instractot/results' },
-  { name: 'Quetchans', icon: <AddressBook/> , path:'/Instractot/Quetchans'},
+
+const menuItems: MenuItem[] = [
+  { name: 'Dashboard', icon: <FaHome />, path: '/Instractot/dashboard' },
+  { name: 'Students', icon: <FaUsers />, path: '/Instractot/students' },
+  { name: 'Groups', icon: <MdGroups />, path: '/Instractot/groups' },
+  { name: 'Quizzes', icon: <FaChalkboardTeacher />, path: '/Instractot/quizzes' },
+  { name: 'Results', icon: <FaChartBar />, path: '/Instractot/results' },
+  { name: 'Quetchans', icon: <AddressBook />, path: '/Instractot/Quetchans' },
 ]
-export default function RootLayout({
+
+export default function InstractotLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
- 
-  const [open, setOpen] = useState(false);
-  const [menu , setMenu] = useState(menuItems)
+}: Readonly<{ children: React.ReactNode }>) {
+  const [open, setOpen] = useState(false)
+  const [menu, setMenu] = useState(menuItems)
 
   return (
-    <html>
-      <body>
-        
-        <Navbar open={open} setOpen={setOpen} menuItems={menu}/>
-        <div className="flex gap-2">
-       
-     
-          <Sidebar setMenu={setMenu} open={open} setOpen={setOpen} menuItems={menu} />
-          <main className="p-4 w-full"><Protected_roote ROLE='Instructor' >{children}</Protected_roote></main>
-        
-         
-        </div>
-
-
-      </body>
-    </html>
-  );
+    <div className="min-h-screen flex flex-col">
+      <Navbar open={open} setOpen={setOpen} menuItems={menu} />
+      <div className="flex flex-1 gap-2">
+        <Sidebar setMenu={setMenu} open={open} setOpen={setOpen} menuItems={menu} />
+        <main className="p-4 w-full">
+          <Protected_roote ROLE="Instructor">{children}</Protected_roote>
+        </main>
+      </div>
+    </div>
+  )
 }
